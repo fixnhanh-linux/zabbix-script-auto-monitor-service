70b2a8578f2347804fac8c3afb1d34d059102048
fixnhanh-linux <tranquangkhai02042000@gmail.com>
2025-03-18 16:16:49 +0700

    Update discover_services.sh

--- discover_services.sh
+++ discover_services.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-SERVICES=("mysql")
+SERVICES=("nginx" "mysql" "apache2" "ssh" "cron")
 
 if [[ "$1" == "discover" ]]; then
     echo -n '{"data":['
     for i in "${!SERVICES[@]}"; do
         if [[ $i -gt 0 ]]; then echo -n ','; fi
-        echo -n '{"{#SERVICE_NAME}":"'${SERVICES[$i]}'"}'
+        echo -n '{"{#SERVICE_NAME}":"'${SERVICES[$i]^^}'"}'
     done
     echo -n ']}'
-elif [[ "$1" == "status" ]]; then
-    systemctl is-active "$2" >/dev/null 2>&1 && echo 1 || echo 0
+elif [[ "$1" == "status" && -n "$2" ]]; then
+    SERVICE_NAME_LOWER=$(echo "$2" | tr '[:upper:]' '[:lower:]')
+    systemctl is-active "$SERVICE_NAME_LOWER" >/dev/null 2>&1 && echo 1 || echo 0
 else
     echo "Usage: $0 discover | status <service_name>"
     exit 1
 fi
-# by to fixnhanh-linux
+# to be trankhai
