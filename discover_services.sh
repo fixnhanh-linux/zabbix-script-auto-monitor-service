7b4463b1229dc55140cee5575b674aea33b30947
fixnhanh-linux <tranquangkhai02042000@gmail.com>
2025-03-18 07:46:19 +0700

    discover_services.sh

---
+++ discover_services.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SERVICES=("nhập dịch vụ cần monitor trạng thái service")
+
+if [[ "$1" == "discover" ]]; then
+    echo -n '{"data":['
+    for i in "${!SERVICES[@]}"; do
+        if [[ $i -gt 0 ]]; then echo -n ','; fi
+        echo -n '{"{#SERVICE_NAME}":"'${SERVICES[$i]}'"}'
+    done
+    echo -n ']}'
+elif [[ "$1" == "status" ]]; then
+    systemctl is-active "$2" >/dev/null 2>&1 && echo 1 || echo 0
+else
+    echo "Usage: $0 discover | status <service_name>"
+    exit 1
+fi
+# by to fixnhanh-linux
